12e5e2343baa5a09118cbdb1a18d6a074f161922
CJ van den Berg <cj@vdbonline.com>
2013-02-07 17:03:30 +0100

    Add pre-built GmpDerivedConstants.h headers

---
+++ patches/gmp-android-14-x86-4.7-GmpDerivedConstants.h
@@ -0,0 +1,12 @@
+/* This file is created automatically.  Do not edit by hand.*/
+
+#define SIZEOF_MP_INT 12
+#define OFFSET_MP_INT__mp_alloc 0
+#define REP_MP_INT__mp_alloc b32
+#define MP_INT__mp_alloc(__ptr__)  REP_MP_INT__mp_alloc[__ptr__+OFFSET_MP_INT__mp_alloc]
+#define OFFSET_MP_INT__mp_size 4
+#define REP_MP_INT__mp_size b32
+#define MP_INT__mp_size(__ptr__)  REP_MP_INT__mp_size[__ptr__+OFFSET_MP_INT__mp_size]
+#define OFFSET_MP_INT__mp_d 8
+#define REP_MP_INT__mp_d b32
+#define MP_INT__mp_d(__ptr__)  REP_MP_INT__mp_d[__ptr__+OFFSET_MP_INT__mp_d]
